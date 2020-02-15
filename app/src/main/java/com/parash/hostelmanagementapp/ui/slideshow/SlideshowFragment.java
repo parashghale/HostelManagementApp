32c317b825d5b96607a8b20d974bf6bc30268c8c
parashghale <parash.ghale20@gmail.com>
2020-02-16 05:40:44 +0545

    Addeed room feature and displayed it

--- app/src/main/java/com/parash/hostelmanagementapp/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/parash/hostelmanagementapp/ui/slideshow/SlideshowFragment.java
@@ -11,19 +11,37 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.parash.hostelmanagementapp.Adapter.RoomAdapter;
+import com.parash.hostelmanagementapp.Adapter.StudentAdapter;
+import com.parash.hostelmanagementapp.Model.Room;
+import com.parash.hostelmanagementapp.Model.Student;
 import com.parash.hostelmanagementapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
-
+    private RecyclerView recyclerView;
+    private List<Room> roomList;
+    private RoomAdapter roomAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
+        recyclerView=root.findViewById(R.id.roomRecycleView);
+        roomList = new ArrayList<>();
+        roomList.add(new Room("Platinum room","Single person room","15000",R.drawable.single));
+        roomList.add(new Room("Premium room","Two person room","12000",R.drawable.two));
+        roomList.add(new Room("Deluxe room","Three person room","10000",R.drawable.three));
+        roomList.add(new Room("Normal room","Four person room","8000",R.drawable.four));
+        roomAdapter= new RoomAdapter(getContext(),roomList);
+        recyclerView.setAdapter(roomAdapter);
         return root;
     }
 }
