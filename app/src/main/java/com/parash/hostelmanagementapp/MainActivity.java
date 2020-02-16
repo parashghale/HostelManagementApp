0899be70c26ae04c2a3f363d284c4354abed22fa
parashghale <parash.ghale20@gmail.com>
2020-02-16 22:32:59 +0545

    updated

--- app/src/main/java/com/parash/hostelmanagementapp/MainActivity.java
+++ app/src/main/java/com/parash/hostelmanagementapp/MainActivity.java
@@ -60,10 +60,5 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
