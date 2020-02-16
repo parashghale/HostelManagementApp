523ac21df338e5ac162c796dd90511fb6f4433d4
parashghale <parash.ghale20@gmail.com>
2020-02-16 22:31:15 +0545

    updated

--- app/src/main/java/com/parash/hostelmanagementapp/ProfileActivity.java
+++ app/src/main/java/com/parash/hostelmanagementapp/ProfileActivity.java
@@ -59,7 +59,13 @@ public class ProfileActivity extends AppCompatActivity {
     private StorageTask uploadsTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        imageView=findViewById(R.id.ProfileImage);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageReference = FirebaseStorage.getInstance().getReference("Uploads");
+        progressBar=findViewById(R.id.progressbar);
+        textView=findViewById(R.id.profileEmail);
 
         textView.setText(firebaseUser.getEmail());
         deleteButton=findViewById(R.id.btnDeleteAccount);
