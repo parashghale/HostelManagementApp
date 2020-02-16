27f053dd454fd2ae6a84c6002022cea62b8b56bd
parashghale <parash.ghale20@gmail.com>
2020-02-16 08:47:15 +0545

    Adding splash screen and checking connectivity

---
+++ app/src/main/java/com/parash/hostelmanagementapp/SplashScreen.java
@@ -0,0 +1,63 @@
+package com.parash.hostelmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+       
+        boolean check = checkConnection();
+        if(check)
+        {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },1000);
+        }
+        else 
+        {
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private boolean checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo network = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+
+        if (wifi.isConnected()) {
+
+            return true;
+
+        }
+        else if (network.isConnected()) {
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+
+    }
+}
+
