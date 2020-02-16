9cdbebf95950cb8ed5fd23f4dc77280a812979b9
parashghale <parash.ghale20@gmail.com>
2020-02-16 22:43:40 +0545

    updated

--- app/src/main/java/com/parash/hostelmanagementapp/LogoutActivity.java
+++ app/src/main/java/com/parash/hostelmanagementapp/LogoutActivity.java
@@ -11,7 +11,4 @@ public class LogoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
         moveTaskToBack(true);
-        android.os.Process.killProcess(android.os.Process.myPid());
-        finish();
-    }
-}
+        andr
